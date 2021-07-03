8e76ea77da7bcf3b9622e53a5635a87b6cb8b9f4
DevHÿllen <jquievreux.devpro@gmail.com>
2021-07-03 18:13:59 +0200

    start setup

---
+++ Footlogs/frontend/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      FootLogs Client Here!!!
+    </div>
+  );
+}
+
+export default App;
---
+++ Footlogs_admin/frontend/src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     FootLogs Admin Here!!!
+    </div>
+  );
+}
+
+export default App;
